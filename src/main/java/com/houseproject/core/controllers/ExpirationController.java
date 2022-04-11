fe9d41526558f13cff2ee3acfac2b0066a5f5c1f
Felipe Parrondo Bastos <felipe.bastos@valtech.com>
2022-04-11 16:20:47 -0300

    Expiration endpoints done

--- src/main/java/com/houseproject/core/controllers/ExpirationController.java
+++ src/main/java/com/houseproject/core/controllers/ExpirationController.java
@@ -1,9 +1,16 @@
 package com.houseproject.core.controllers;
 
+import com.houseproject.core.entities.ExpirationEntity;
 import com.houseproject.core.services.IExpirationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/expiration")
@@ -14,5 +21,31 @@ public class ExpirationController {
     @Autowired
     public ExpirationController(IExpirationService service) {
         this.service = service;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ExpirationEntity>> findAll() {
+        return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ExpirationEntity> getExpirationProductById(@PathVariable(value = "id") BigInteger id) {
+        return new ResponseEntity<>(service.getExpirationProductById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/{date}")
+    public ResponseEntity<List<ExpirationEntity>> getExpirationProductByExpirationDate(@PathVariable(value = "date")LocalDate date) {
+        return new ResponseEntity<>(service.getExpirationProductByExpirationDate(date), HttpStatus.OK);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ExpirationEntity> createExpirationProduct(ExpirationEntity product) {
+        return new ResponseEntity<>(service.createExpirationProduct(product), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteExpirationProduct(@PathVariable(value = "id") BigInteger id) {
+        service.deleteExpirationProduct(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
